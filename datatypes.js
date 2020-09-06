8486fff8e8d30e1b01ee39073027289a27fcef8c
restuaji623 <restupangestuaji4@gmail.com>
2020-09-06 16:05:46 +0700

    first commit

---
+++ datatypes.js
@@ -0,0 +1,9 @@
+const nama = 'restu pangestu aji' ;//string
+let umur = 16 ;//
+var alamat = 'pondok kopi' ;//string
+let lulus = true ; //boolean
+console.log(`${nama} umur ${umur} alamat ${alamat}`);
+
+
+
+
